31d3e5437e5a68877ffeac7b888a1bd6aae9fdda
bhunao <bhunao@gmail.com>
2021-11-16 19:39:11 -0300

    variable name

--- api/endpoints.py
+++ api/endpoints.py
@@ -6,17 +6,17 @@ except:
 from flask import json, request
 
 
-api, mycache = create_app()
+api, cache = create_app()
 
 @api.route('/temperature/<city>', methods=['GET'])
 def get_weather(city: str):
-    weather = mycache.get(city)
+    weather = cache.get(city)
     if weather is None:
         data = open_weather_api.get_data(city, endpoint=endpoint, key=key)
         weather = open_weather_api.transform_data(data)
         if weather == "error":
             return json.dumps({"message": "City not found."}), 401
-        mycache.set(city, weather)
+        cache.set(city, weather)
         return json.dumps(weather)
     return json.dumps(weather)
 
@@ -25,8 +25,8 @@ def get_cached_weather():
     # query parameter
     max_number = int(request.args.get('max', default_max_number))
     
-    keys = mycache.keys
-    r = [{k: mycache.get(k)} for k in keys]
+    keys = cache.keys
+    r = [{k: cache.get(k)} for k in keys]
     rsult = [re for re in reversed(r)]
 
     if len(rsult) > max_number:
